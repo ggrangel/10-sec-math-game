946746c0f7739d2dbecedcec73c2a3efde3a405a
Gustavo Rangel <gustavorangel91@gmail.com>
2022-03-28 10:04:38 -0300

    Fix division operation to have only whole numbers as answer.

--- js/application.js
+++ js/application.js
@@ -1,9 +1,9 @@
-let Game = function(maxN) {
+let Game = function (maxN) {
   this.score = 0;
   this.gameStart = false;
   this.maxN = maxN;
 
-  this.newOperation = function() {
+  this.newOperation = function () {
     let op = _.sample(["+", "-", "*", "/"]);
     $("#operation").html(op);
     var n1 = _.random(1, this.maxN);
@@ -22,7 +22,8 @@ let Game = function(maxN) {
         this.res = n1 * n2;
         break;
       case "/":
-        var n2 = _.random(1, n1);
+        var n2 = _.random(1, 10);
+        var n1 = _.random(1, 10) * n2;
         this.res = parseInt(n1 / n2);
         break;
     }
@@ -30,15 +31,15 @@ let Game = function(maxN) {
     $("#n1").html(n1);
     $("#n2").html(n2);
   };
-  this.evaluateResult = function(answer) {
+  this.evaluateResult = function (answer) {
     return answer === this.res;
   };
-  this.increaseScore = function() {
+  this.increaseScore = function () {
     this.score++;
   };
 };
 
-$(document).on("keypress", function(e) {
+$(document).on("keypress", function (e) {
   if (e.which === 13) {
     resultInput = $("#result");
     correct = game.evaluateResult(parseInt(resultInput.val()));
@@ -55,27 +56,27 @@ $(document).on("keypress", function(e) {
   }
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   HIGHEST = 0;
 
   res = game.newOperation();
 
   $("#result").focus();
-  $("#result").on("keyup", function() {
+  $("#result").on("keyup", function () {
     startGame();
   });
 });
 
-var initGame = function(maxN) {
+var initGame = function (maxN) {
   game = new Game(maxN);
 };
 
 var timeLeft = 10;
 var interval;
-var startGame = function() {
+var startGame = function () {
   if (!interval) {
     $("#maxNumber").prop("disabled", true);
-    interval = setInterval(function() {
+    interval = setInterval(function () {
       timeLeft--;
       display = $("#sec-left");
       display.text(timeLeft);
@@ -97,7 +98,7 @@ var startGame = function() {
 
 initGame(10);
 
-var playAgain = function() {
+var playAgain = function () {
   $("#result").focus();
   $("#result").val("");
   $("#result").prop("disabled", false);
@@ -111,7 +112,7 @@ var playAgain = function() {
   display.text(timeLeft);
 };
 
-var rangeUpdate = function() {
+var rangeUpdate = function () {
   maxN = $("#maxNumber").val();
   game.maxN = maxN;
   $("#lblMaxN").html(maxN);
